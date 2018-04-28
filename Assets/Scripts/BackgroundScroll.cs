e60a333df8b8a6018c68cd732407e3f6a46b314b
DennisLube <denya95@gmail.com>
2018-04-28 16:27:48 -0400

    background stuff

--- Assets/Scripts/BackgroundScroll.cs
+++ Assets/Scripts/BackgroundScroll.cs
@@ -4,21 +4,16 @@ using UnityEngine;
 
 public class BackgroundScroll : MonoBehaviour {
 
+    public float offset = 100;
     public float moveSpeed = -10;
     public Transform player;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
 	void Update () {
         transform.position += new Vector3(0, 0, Time.deltaTime * moveSpeed);
 
         if (transform.position.z < player.position.z)
         {
-            transform.position += new Vector3(0, 0, 100);
+            transform.position += new Vector3(0, 0, offset);
         }
     }
 }
